ea951471ad8b5238a4265ea7202fb72b5e08810e
az8321550 <az8321550@gmail.com>
2016-05-17 18:57:12 +0800

    Optimize

--- src/routes/Zen/components/Zen.js
+++ src/routes/Zen/components/Zen.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React,{Component} from 'react'
 import classes from './Zen.scss'
 
 const child=(item)=>(
   <h1 key={item.id}>{item.text}</h1>
 )
 
-export default (props)=>(
-  <div className={classes.zen}>
-    <button class='btn btn-default' onClick={props.fetchZen}>{props.zen.fetching?'Fetching...':'Fetch'}</button>&nbsp;&nbsp;
-    <button class='btn btn-default' onClick={props.clearZen}>Clear</button>
-    <div>
-      {props.zen.text.map(child)}
-    </div>
-  </div>
-)
+export default class Zen extends Component{
+
+  render(){
+    const props=this.props;
+    return (
+      <div className={classes.zen}>
+        <button class='btn btn-default' onClick={props.fetchZen}>{props.zen.fetching?'Fetching...':'Fetch'}</button>&nbsp;&nbsp;
+        <button class='btn btn-default' onClick={props.clearZen}>Clear</button>
+        <div>
+          {props.zen.text.map(child)}
+        </div>
+      </div>
+    )
+  }
+}

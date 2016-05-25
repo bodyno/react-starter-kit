5c01f631fa0091c4fd491820deec5ae1aa4790d8
az8321550 <az8321550@gmail.com>
2016-05-25 11:39:33 +0800

    Nicety update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Redux Starter Kit</h1>
+    <h1>React Starter Kit ---- <a href="https://bodyno.com" target="_blank">bodyno</a></h1>
     <IndexLink to='/' activeClassName={classes.activeRoute}>
       Home
     </IndexLink>
--- src/routes/Zen/components/Zen.js
+++ src/routes/Zen/components/Zen.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
 import classes from './Zen.scss'
+import Loader from 'halogen/RingLoader'
+import { If, Then, Else } from 'react-if';
 
 const child=(item)=>(
   <h1 key={item.id}>{item.text}</h1>
@@ -9,10 +11,20 @@ export default class Zen extends Component{
 
   render(){
     const props=this.props;
+
     return (
       <div className={classes.zen}>
-        <button class='btn btn-default' onClick={props.fetchZen}>{props.zen.fetching?'Fetching...':'Fetch'}</button>&nbsp;&nbsp;
-        <button class='btn btn-default' onClick={props.clearZen}>Clear</button>
+        <div>
+          <button class='btn btn-default' onClick={props.fetchZen}>{props.zen.fetching?'Fetching...':'Fetch'}</button>&nbsp;&nbsp;
+          <button class='btn btn-default' onClick={props.clearZen}>Clear</button>
+        </div>
+        <If condition={props.zen.fetching}>
+          <Then>
+            <div className={classes.loader}>
+              <Loader color="#26A65B" />
+            </div>
+          </Then>
+        </If>
         <div>
           {props.zen.text.map(child)}
         </div>

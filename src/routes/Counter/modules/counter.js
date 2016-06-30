3f2ee034604191acd710bcc7ba00ad8d1ed905bd
bodyno <az8321550@gmail.com>
2016-06-30 18:55:44 +0800

    Updated counter export

--- src/routes/Counter/modules/counter.js
+++ src/routes/Counter/modules/counter.js
@@ -1,7 +1,7 @@
 // ------------------------------------
 // Constants
 // ------------------------------------
-const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
+export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
 
 // ------------------------------------
 // Actions
@@ -12,6 +12,14 @@ export function increment (value = 1) {
     payload: value
   }
 }
+
+/*  This is a thunk, meaning it is a function that immediately
+    returns a function for lazy evaluation. It is incredibly useful for
+    creating async actions, especially when combined with redux-thunk!
+
+    NOTE: This is solely for demonstration purposes. In a real application,
+    you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
+    reducer take care of this logic.  */
 
 export const doubleAsync = () => {
   return (dispatch, getState) => {
@@ -24,20 +32,23 @@ export const doubleAsync = () => {
   }
 }
 
+export const actions = {
+  increment,
+  doubleAsync
+}
+
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [COUNTER_INCREMENT]: (state, action) => {
-    return state + action.payload
-  }
+  [COUNTER_INCREMENT]: (state, action) => state + action.payload
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = 0
-export default function (state = initialState, action) {
+export default function counterReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
 
   return handler ? handler(state, action) : state

fad4fdd092ad6318feb24ae14cbae30bcd2d715f
bodyno <az8321550@gmail.com>
2016-07-26 21:44:44 +0800

    feat(core): explicitly unmount hot routes

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,23 +1,22 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Router } from 'react-router'
 import { Provider } from 'react-redux'
 
-class AppContainer extends React.Component {
+class AppContainer extends Component {
   static propTypes = {
     history: PropTypes.object.isRequired,
     routes: PropTypes.object.isRequired,
-    routerKey: PropTypes.number,
     store: PropTypes.object.isRequired
   }
 
   render () {
-    const { history, routes, routerKey, store } = this.props
+    const { history, routes, store } = this.props
     const pageChange = () => window.scrollTo(0, 0)
 
     return (
       <Provider store={store}>
         <div style={{ height: '100%' }}>
-          <Router history={history} children={routes} key={routerKey} onUpdate={pageChange} />
+          <Router history={history} children={routes} onUpdate={pageChange} />
         </div>
       </Provider>
     )
